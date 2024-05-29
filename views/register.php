29ef3e66b26278ff5301c14124ad8b46e7e41eca
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-05-29 20:19:19 +0800

    edited register form

--- views/register.php
+++ views/register.php
@@ -4,39 +4,39 @@ include '../includes/header.php';
 ?>
 
 <div class="bg-gray-100 py-16 mt-16 mb-7 flex justify-center">
-    <div class="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg mt-7">
-        <h2 class="unna-style text-4xl font-bold text-center pb-4 border-b-2 border-gray-500 ">Register</h2>
-        <form action="../assets/config/Register.php" method="POST" class="space-y-4 pt-4">
+    <div class="bg-white rounded-lg shadow-lg w-full max-w-lg mt-7 pb-2">
+        <h2 class="unna-style text-4xl font-bold text-center py-4 border-b-2 border-gray-500 ">Register</h2>
+        <form action="../assets/config/Register.php" method="POST" class="space-y-2 px-12 pt-8">
             <div>
-                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-                <input type="text" id="username" name="username" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
+                <label for="nama" class="block text-sm font-medium text-gray-700">Nama</label>
+                <input type="text" id="nama" name="nama" class="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
             </div>
             <div>
-                <label for="username" class="block text-sm font-medium text-gray-700">Email</label>
-                <input type="email" id="email" name="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
+                <label for="notelp" class="block text-sm font-medium text-gray-700">No. Telp</label>
+                <input type="text" id="notelp" name="notelp" class="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
+            </div>
+            <div>
+                <label for="alamat" class="block text-sm font-medium text-gray-700">Alamat</label>
+                <input type="text" id="alamat" name="alamat" class="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
+            </div>
+            <div>
+                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                <input type="text" id="username" name="username" class="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
             </div>
             <div>
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
-                <input type="password" id="password" name="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
+                <input type="password" id="password" name="password" class="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm" required>
             </div>
-            <div>
-                <a href="#" class="block w-full text-center py-2 px-4 bg-green-500 text-white font-semibold rounded-md shadow hover:bg-green-600">
+            <div class="pt-2">
+                <a href="#" class="block w-full text-center py-1 px-4 bg-green-500 text-white font-semibold rounded-md shadow hover:bg-green-600">
                     Register
                 </a>
             </div>
         </form>
         <div class="relative my-4">
-            <div class="absolute inset-0 flex items-center">
-                <div class="w-full border-t border-gray-300"></div>
-            </div>
             <div class="relative flex justify-center text-sm">
-                <span class="px-2 bg-white text-gray-500">Already have Account?</span>
+                <span class="px-2 bg-white text-gray-500">Already have an account?</span><a href="login.php" class="text-green-500 hover:text-green-600">Login</a>
             </div>
-        </div>
-        <div>
-            <a href="login.php" class="block w-full text-center py-2 px-4 bg-transparent text-black font-semibold rounded-md border-2 border-green-500 shadow hover:bg-gray-100">
-                Login
-            </a>
         </div>
     </div>
 </div>
