832e3c0e53ca5a9942b4ca0e77e9dce63e7c15fd
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-06-24 00:39:26 +0800

    important: added /includes to tailwindcss's content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: [
     "./views/*.php",
+    "./includes/*.php",
     "./index.php"
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        containerShadow: '0 8px 23px rgba(80, 107, 82, 0.13)',
+      }
+    },
   },
   plugins: [],
 }
