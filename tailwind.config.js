dc3ca590639876618981672cdb747035b7058a41
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-06-24 00:40:27 +0800

    added mainText (section title) color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "mainText": "rgba(52, 52, 52, 0.8)",
+      },
       dropShadow: {
         containerShadow: '0 8px 23px rgba(80, 107, 82, 0.13)',
       }
