336826b2ea21973f91b57368f3853985579b169b
prmnaa30 <73104057+prmnaa30@users.noreply.github.com>
2024-07-04 19:58:53 +0800

    changes: added a query to get user data

--- assets/config/views.php
+++ assets/config/views.php
@@ -4,8 +4,11 @@ include 'Config.php';
 
 // Query untuk mengambil 10 data terbaru dari view admin_orders_view
 $sql = "SELECT * FROM admin_orders_view ORDER BY order_date DESC LIMIT 10";
+// Query untuk mengambil data user
+$get_user_data = "SELECT name, phone, alamat, username, created_at FROM users WHERE role = 'customer' ORDER BY created_at DESC";
 
 // Eksekusi query
-$result = $conn->query($sql);
+$order_result = $conn->query($sql);
+$user_result = $conn->query($get_user_data);
 
 ?>
